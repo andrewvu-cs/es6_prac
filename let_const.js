358a4062c3f3e1aa3eb14e9bb036057435b19d1e
avu916 <andrewvu.cs@gmail.com>
2019-05-03 17:29:39 -0700

    variables

---
+++ let_const.js
@@ -0,0 +1,17 @@
+// LET & CONST VARIABLES
+
+// let - for only variables
+// const - const for values that should never change
+
+let myName = 'Andrew';     
+console.log(myName);
+
+myName = 'Andres';
+console.log(myName);
+
+//Code below will provide an error because myName value cannot be reassigned
+const myName = 'Andrew';
+console.log(myName);
+
+myName = 'Andres';
+console.log(myName);
