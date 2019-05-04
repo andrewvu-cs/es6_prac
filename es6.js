e3584f08ef455b63cc2d45bb1602ccb372de164d
avu916 <andrewvu.cs@gmail.com>
2019-05-03 17:25:11 -0700

    classes

--- es6.js
+++ es6.js
@@ -57,6 +57,7 @@ const person = {
 }
 export default person
 //app.js 
+//You can choose the name  of the import on export default
 import person from './person.js'
 import prs from './person.js'
 
@@ -65,9 +66,73 @@ import prs from './person.js'
 export const clean = () => {...}
 export const baseData = 10;
 //app.js
+
 import{baseData} from './utility.js'
 import{clean} from './utility.js'
 //OR
 import{baseData,clean} from './utility.js'
+//Have to used the exact name unless
+import{something as thing} from './utility.js'
+import * as allThings from './utility.js'
 
 
+//Classes
+//Inheritance
+class Human {
+    constructor(){
+      this.gender = 'male';
+    }
+    printGender(){
+      console.log(this.gender);
+    }
+  }
+  
+class Person extends Human{
+    constructor(){
+      super();
+      this.name ='Max';
+      this.gender = 'female';
+    }
+    printMyName(){  
+   console.log(this.name);
+   }
+}
+  
+  const person = new Person;
+  person.printMyName();
+  person.printGender();
+
+  //CLasses, Properties, Methods
+  //Properties are like "variables attached to classes/objects"
+  //ES6
+  constructor(){
+      this.myProperty = 'value'
+  }
+  //ES7
+  myProperty = 'value'
+  //Methods are like "functions attached to classes/objects"
+  //ES6
+  myMethod(){...}
+  //ES7
+  myMethod = () => {...}
+
+
+class Human {
+    gender = 'male';
+    printGender = () => {
+      console.log(this.gender);
+    }
+  }
+  
+  class Person extends Human{
+    name = 'Andrew';
+    gender = 'female';
+  
+    printMyName = () => {
+      console.log(this.name);
+    }
+  }
+  
+  const person = new Person;
+  person.printMyName();
+  person.printGender();
